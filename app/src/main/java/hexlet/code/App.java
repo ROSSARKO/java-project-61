5e0b53175248f1cee80f6042a07882b4db6b7319
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-06 20:12:31 +0300

    upd App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Cli.userGreeting();
+        String[] gamesList = {"1 - Greet", "2 - Even", "0 - Exit"};
+        Scanner sc = new Scanner(System.in);
+        String formattedGamesList = Arrays.toString(gamesList)
+                        .replace("[","")
+                                .replace(",","\n")
+                                        .replace("]","")
+                                                .replace(" ","");
+        System.out.println("Please enter the game number and press Enter");
+        System.out.println(formattedGamesList);
+        int gameNumber = sc.nextInt();
+        switch (gameNumber) {
+            case 0: System.console();
+                break;
+            case 1: Cli.userGreeting();
+                break;
+            case 2: EvenGame.gameLogic();
+                break;
+        }
     }
 }
