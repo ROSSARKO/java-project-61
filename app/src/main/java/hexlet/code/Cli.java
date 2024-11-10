ac7d5b3d069408123da2d92afc44ec20fb9b46fb
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-10 18:48:56 +0300

    upd Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,13 +1,7 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Cli {
     public static void userGreeting() {
-        Scanner sc = new Scanner(System.in);
-        String userName = sc.next();
-        System.out.println("Hello " + userName + "!");
-        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
-        System.out.println("Hello " + userName + "!");
+        Engine.userGreetings();
     }
 }
