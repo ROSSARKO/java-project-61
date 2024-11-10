841d75d62ec20b66dabfcf31ce4470dfd092bb25
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-10 16:28:39 +0300

    add Prime.java

---
+++ app/src/main/java/hexlet/code/Prime.java
@@ -0,0 +1,37 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prime {
+    public static void gameLogic() {
+        Scanner sc = new Scanner(System.in);
+        Random randNum = new Random();
+        boolean isPrime = true;
+        Engine.userGreetings();
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+        while (Engine.questionCounter != 3) {
+            int num = randNum.nextInt(2,1000);
+            System.out.println("Question: " + num);
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if(num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            String answer = sc.next();
+            if (answer.equals("yes") && isPrime) {
+                System.out.println("Correct!\n");
+                Engine.questionCounter++;
+            } else if (answer.equals("no") && !isPrime) {
+                System.out.println("Correct!\n");
+                Engine.questionCounter++;
+            } else {
+                System.out.println("This is not correct answer,try again!\n");
+            }
+            if(Engine.questionCounter == 3) {
+                System.out.println("Congratulations! " + Engine.userName);
+            }
+        }
+    }
+}
