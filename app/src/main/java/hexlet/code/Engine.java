c7d8e0b8ee19e458b0d81743dcd13154f713137f
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-10 19:52:28 +0300

    upd Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,41 +6,53 @@ import java.util.Scanner;
 public class Engine {
     public static String userName = "";
     public static int questionCounter = 0;
+
     public static void userGreetings() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         userName = sc.next();
         System.out.println("Hello, " + userName + "!");
     }
+
     public static void selectGame() {
         String[] gamesList = {"1 - Greet", "2 - Even", "3 - Calc", "4 - GCD",
                 "5 - Progression", "6 - Prime", "0 - Exit"};
         Scanner sc = new Scanner(System.in);
         String formattedGamesList = Arrays.toString(gamesList)
-                .replace("[","")
-                .replace(",","\n")
-                .replace("]","")
-                .replace(" ","");
+                .replace("[", "")
+                .replace(",", "\n")
+                .replace("]", "")
+                .replace(" ", "");
         System.out.println("Please enter the game number and press Enter");
         System.out.println(formattedGamesList);
         int gameNumber = sc.nextInt();
         switch (gameNumber) {
-            case 0: System.console();
+            case 0:
+                System.console();
                 break;
-            case 1: Cli.userGreeting();
+            case 1:
+                Cli.userGreeting();
                 break;
-            case 2: EvenGame.gameLogic();
+            case 2:
+                EvenGame.gameLogic();
                 break;
-            case 3: Calc.gameLogic();
+            case 3:
+                Calc.gameLogic();
                 break;
-            case 4: GCD.gameLogic();
+            case 4:
+                GCD.gameLogic();
                 break;
-            case 5: Progression.gameLogic();
+            case 5:
+                Progression.gameLogic();
                 break;
-            case 6: Prime.gameLogic();
+            case 6:
+                Prime.gameLogic();
                 break;
+            default:
+                System.console();
         }
     }
+
     public static int gcdGenerate(int a, int b) {
         while (b != 0) {
             int temp = a % b;
