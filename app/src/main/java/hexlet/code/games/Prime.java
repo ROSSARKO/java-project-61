2e6de4307097c65a4bfff5aefa3404914b3704dd
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-11 20:20:18 +0300

    refactor: logic change

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,41 +1,31 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class Prime {
-    public static void gameLogic() {
+    public static void startGame() {
         final int rndMax = 1000;
-        Scanner sc = new Scanner(System.in);
         Random randNum = new Random();
-        Engine.userGreetings();
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-        while (Engine.getQuestionCounter() != Engine.getMaxQuestions()) {
+        String[][] roundsData = new String[Engine.ROUNDS][2];
+        String description = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+        for (int i = 0; i < Engine.ROUNDS; i++) {
             int num = randNum.nextInt(2, rndMax);
             boolean isPrime = true;
-            System.out.println("Question: " + num);
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
+
+            for (int j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
                     isPrime = false;
                     break;
                 }
             }
-            System.out.print("Your answer: ");
-            String answer = sc.next();
-            if (answer.equals("yes") && isPrime) {
-                System.out.println("Correct!");
-                Engine.incrementQuestionsCounter();
-            } else if (answer.equals("no") && !isPrime) {
-                System.out.println("Correct!");
-                Engine.incrementQuestionsCounter();
-            } else {
-                System.out.println("Let's try again, " + Engine.getUserName() + "!");
-                break;
-            }
-            if (Engine.getQuestionCounter() == Engine.getMaxQuestions()) {
-                System.out.println("Congratulations, " + Engine.getUserName() + "!");
-            }
+            String answer = isPrime ? "yes" : "no";
+
+            roundsData[i][0] = String.valueOf(num);
+            roundsData[i][1] = answer;
         }
+        Engine.run(description, roundsData);
     }
 }
