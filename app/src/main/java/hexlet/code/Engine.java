904127ba1578b32ab41379be7946bf1cbd43cfeb
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-11 20:27:18 +0300

    chore: correcting a message to user

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -24,7 +24,7 @@ public class Engine {
             } else {
                 System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer "
                     + "was '" + correctAnswer + "'");
-                System.out.println("Let's try again, " + userName);
+                System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
