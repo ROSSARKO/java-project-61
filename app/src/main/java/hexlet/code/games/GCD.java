80f372bb511fb8beeeb7dc59e589bf212794be34
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-11 20:20:17 +0300

    refactor: logic change

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,36 +1,33 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class GCD {
-    public static void gameLogic() {
+    public static void startGame() {
         final int maxValue = 100;
-        Engine.userGreetings();
-        System.out.println("Find the greatest common divisor of given numbers.");
-        Scanner sc = new Scanner(System.in);
-        int result;
-        while (Engine.getQuestionCounter() != Engine.getMaxQuestions()) {
-            Random randNum = new Random();
+        String[][] roundsData = new String[Engine.ROUNDS][2];
+        String description = "Find the greatest common divisor of given numbers.";
+        Random randNum = new Random();
+
+        for (int i = 0; i < Engine.ROUNDS; i++) {
             int num1 = randNum.nextInt(maxValue);
             int num2 = randNum.nextInt(maxValue);
-            System.out.println("Question: " + num1 + " " + num2);
-            int answer = sc.nextInt();
-            result = Engine.gcdGenerate(num1, num2);
-            if (answer == result) {
-                System.out.println("Correct!\n");
-                Engine.incrementQuestionsCounter();
-            } else {
-                System.out.println("Your answer: " + answer);
-                System.out.println("'" + answer + "'" + "is wrong answer ;(. "
-                        + "Correct answer was" + "'" + result + "'");
-                System.out.println("Let's try again, " + Engine.getUserName() + "!");
-                break;
-            }
-            if (Engine.getQuestionCounter() == Engine.getMaxQuestions()) {
-                System.out.println("Congratulations, " + Engine.getUserName() + "!");
-            }
+            String question = num1 + " " + num2;
+            int result = gcdCalculate(num1, num2);
+            roundsData[i][0] = question;
+            roundsData[i][1] = String.valueOf(result);
         }
+        Engine.run(description, roundsData);
+    }
+
+    private static int gcdCalculate(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
     }
 }
