bb5f1cfd53fb429945ed662d93738c559daba501
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-11 20:20:08 +0300

    refactor: logic change

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,44 +3,34 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class Calc {
-    public static void gameLogic() {
+    public static void startGame() {
         final int rndMax = 100;
         final int mathSymbolMax = 3;
-        Engine.userGreetings();
-        Scanner sc = new Scanner(System.in);
         char[] mathSymbols = {'+', '-', '*'};
-        int result = 0;
-        System.out.println("What is the result of the expression?\n");
-        while (Engine.getQuestionCounter() != Engine.getMaxQuestions()) {
-            Random randNum = new Random();
+        String[][] roundsData = new String[Engine.ROUNDS][2];
+        String description = "What is the result of the expression?";
+        Random randNum = new Random();
+
+        for (int i = 0; i < Engine.ROUNDS; i++) {
             int randMathSymbol = randNum.nextInt(mathSymbolMax);
             int num1 = randNum.nextInt(rndMax);
             int num2 = randNum.nextInt(rndMax);
-            System.out.println("Question: " + num1 + " " + mathSymbols[randMathSymbol] + " " + num2);
-            result = switch (mathSymbols[randMathSymbol]) {
+
+            String question = num1 + " " + mathSymbols[randMathSymbol] + " " + num2;
+
+            int result = switch (mathSymbols[randMathSymbol]) {
                 case '+' -> num1 + num2;
                 case '-' -> num1 - num2;
                 case '*' -> num1 * num2;
-                default -> result;
+                default -> 0;
             };
-            int answer = sc.nextInt();
-            if (answer == result) {
-                System.out.println("Correct!\n");
-                Engine.incrementQuestionsCounter();
-            } else {
-                System.out.println("Your answer: " + answer);
-                System.out.println("'" + answer + "'" + "is wrong answer ;(. "
-                        + "Correct answer was" + "'" + result + "'");
-                System.out.println("Let's try again, " + Engine.getUserName() + "!");
-                break;
-            }
-            if (Engine.getQuestionCounter() == Engine.getMaxQuestions()) {
-                System.out.println("Congratulations, " + Engine.getUserName() + "!");
-            }
 
+            roundsData[i][0] = question;
+            roundsData[i][1] = String.valueOf(result);
         }
+        Engine.run(description, roundsData);
     }
 }
