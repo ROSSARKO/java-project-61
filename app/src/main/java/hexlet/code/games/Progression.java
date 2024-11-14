2ef87f13a5fe6f255e3ba5a97866bb66dd94123a
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-14 18:03:24 +0300

    refactor: add new method

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,43 +1,41 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Progression {
     public static void startGame() {
-        final int arrLength = 10;
-        final int stepMax = 10;
-        final int hideElementMax = 9;
-        final int firstNumMax = 100;
         String[][] roundsData = new String[Engine.ROUNDS][2];
-        Random randNum = new Random();
         String description = "What number is missing in the progression?";
 
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int progressionStep = randNum.nextInt(1, stepMax);
-            int randHideElement = randNum.nextInt(0, hideElementMax);
-            int firstNum = randNum.nextInt(1, firstNumMax);
+            int randHideElement = Utils.generateNumber(0, 9);
+            String[] progressionWithDots = new String[getProgression().length];
 
-            int[] progression = new int[arrLength];
-            progression[0] = firstNum;
-            for (int j = 1; j < progression.length; j++) {
-                progression[j] = progression[j - 1] + progressionStep;
-            }
-
-            String[] progressionWithDots = new String[progression.length];
-
-            for (int k = 0; k < progression.length; k++) {
+            for (int k = 0; k < getProgression().length; k++) {
                 if (k == randHideElement) {
                     progressionWithDots[k] = "..";
                 } else {
-                    progressionWithDots[k] = String.valueOf(progression[k]);
+                    progressionWithDots[k] = String.valueOf(getProgression()[k]);
                 }
             }
             String question = String.join(" ", progressionWithDots);
             roundsData[i][0] = question;
-            roundsData[i][1] = String.valueOf(progression[randHideElement]);
+            roundsData[i][1] = String.valueOf(getProgression()[randHideElement]);
         }
         Engine.run(description, roundsData);
+    }
+
+    private static int[] getProgression() {
+        final int arrLength = 10;
+        int progressionStep = Utils.generateNumber(0, 10);
+        int firstNum = Utils.generateNumber(0, 100);
+
+        int[] progression = new int[arrLength];
+        progression[0] = firstNum;
+        for (int j = 1; j < progression.length; j++) {
+            progression[j] = progression[j - 1] + progressionStep;
+        }
+        return progression;
     }
 }
