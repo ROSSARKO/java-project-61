cbd8e5735cc730d5d995e9f7e8de391545e27af7
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-14 18:03:16 +0300

    refactor: add new method

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,31 +1,29 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Prime {
     public static void startGame() {
-        final int rndMax = 1000;
-        Random randNum = new Random();
+
         String[][] roundsData = new String[Engine.ROUNDS][2];
         String description = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int num = randNum.nextInt(2, rndMax);
-            boolean isPrime = true;
-
-            for (int j = 2; j <= Math.sqrt(num); j++) {
-                if (num % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            String answer = isPrime ? "yes" : "no";
-
+            int num = Utils.generateNumber(2,1000);
+            String answer = isPrime(num) ? "yes" : "no";
             roundsData[i][0] = String.valueOf(num);
             roundsData[i][1] = answer;
         }
         Engine.run(description, roundsData);
+    }
+
+    public static boolean isPrime(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
