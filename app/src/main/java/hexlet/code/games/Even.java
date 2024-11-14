f1da5729b7e834d1de92a9b5c135d1436aebd147
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-14 18:02:34 +0300

    refactor: logic correction

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,18 +1,15 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-
-import java.util.Random;
+import hexlet.code.Utils;
 
 public class Even {
     public static void startGame() {
-        final int rndMax = 100;
         String description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         String[][] roundsData = new String[Engine.ROUNDS][2];
-        Random rand = new Random();
 
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            int question = rand.nextInt(rndMax);
+            int question = Utils.generateNumber(1, 100);
             String answer = (question % 2 == 0) ? "yes" : "no";
             roundsData[i][0] = String.valueOf(question);
             roundsData[i][1] = answer;
