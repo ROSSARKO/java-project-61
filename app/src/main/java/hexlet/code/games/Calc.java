d07532b786a3d6748aba06fb150bda3618489e0c
Ilya Sokolov <ilya.rossar@yandex.ru>
2024-11-18 17:23:35 +0300

    refactor: adding an exception

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -6,6 +6,7 @@ import hexlet.code.Utils;
 public class Calc {
     private static final int MIN = 0;
     private static final int MAX = 100;
+
     public static void startGame() {
         String[][] roundsData = new String[Engine.ROUNDS][2];
         String description = "What is the result of the expression?";
@@ -13,28 +14,23 @@ public class Calc {
         for (int i = 0; i < Engine.ROUNDS; i++) {
             int num1 = Utils.generateNumber(MIN, MAX);
             int num2 = Utils.generateNumber(MIN, MAX);
-            char mathSymbol = getRandomMathSymbol();
+            char[] mathSymbols = {'+', '-', '*'};
+            int index = Utils.generateNumber(0, 2);
 
-            String question = num1 + " " + mathSymbol + " " + num2;
+            String question = num1 + " " + mathSymbols[index] + " " + num2;
 
             roundsData[i][0] = question;
-            roundsData[i][1] = String.valueOf(getExpressionResult(mathSymbol, num1, num2));
+            roundsData[i][1] = String.valueOf(calculate(mathSymbols[index], num1, num2));
         }
         Engine.run(description, roundsData);
     }
 
-    private static char getRandomMathSymbol() {
-        char[] mathSymbols = {'+', '-', '*'};
-        int index = Utils.generateNumber(0, 2);
-        return mathSymbols[index];
-    }
-
-    private static int getExpressionResult(char mathSymbol, int numOne, int numTwo) {
-        return switch (mathSymbol) {
-            case '+' -> numOne + numTwo;
-            case '-' -> numOne - numTwo;
-            case '*' -> numOne * numTwo;
-            default -> 0;
+    private static int calculate(char operator, int num1, int num2) {
+        return switch (operator) {
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            default -> throw new RuntimeException("Unknown operator: " + operator);
         };
     }
 }
